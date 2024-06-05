f73f1e879763dbf21670c1e7450800c2e061f651
Martin Otten <martinotten@web.de>
2024-06-05 16:16:16 +0200

    Maze solver to find a way from S to E

---
+++ mazeSolver.hpp
@@ -0,0 +1,45 @@
+#ifndef MAZE_SOLVER_HPP
+#define MAZE_SOLVER_HPP
+
+#include "point.hpp"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class MazeSolver {
+public:
+    // Constructors
+    MazeSolver();
+    MazeSolver(string filename);
+
+    // Destructor
+    ~MazeSolver();
+
+    // Public methods
+    void run();
+    void printMaze();
+    void readMaze(const string &filename);
+    vector<Point> dfs();
+
+private:	
+    // Private methods
+    void searchSandE();
+    vector<Point> get_neighbors(Point position);
+
+
+    // Private members
+    vector<vector<char>> maze;
+    int rows;
+    int cols;
+    vector<Point> solution_points;
+    char wall = ' ';
+    char path = 'X';
+    char start_char = 'S';
+    char end_char = 'E';
+    char solutin_char = 'O';
+    Point start;
+    Point end;
+};
+
+#endif // MAZE_SOLVER_HPP
