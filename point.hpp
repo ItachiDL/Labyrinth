26605ec85ef4eb05db248ab2edc5e9115839fb44
Martin Otten <martinotten@web.de>
2024-06-05 16:15:29 +0200

    Class point to handle 2D points

---
+++ point.hpp
@@ -0,0 +1,37 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+class Point{
+public:
+    // Constructor
+    Point(int x, int y);
+    Point() = default;
+
+    // Destructor
+    ~Point();
+
+    // Getters
+    int getX();
+    int getY();
+
+    int getX() const;
+    int getY() const;
+
+    // Setters
+    void setX(int x);
+    void setY(int y);
+
+    // Operator !=
+    bool operator!=(const Point& other) const;
+    // Operator ==
+    bool operator==(const Point& other) const;
+
+    bool operator<(const Point& other) const;
+
+    bool operator>(const Point& other) const;
+private:
+    int x;
+    int y;
+}; 
+
+#endif // POINT.HPP
